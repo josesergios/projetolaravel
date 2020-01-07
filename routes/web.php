5c0fcb8612787c2c88c69c6a2c440e8f73243104
Sergio Santos <josesergio.s@hotmail.com>
2020-01-07 18:08:19 -0300

    criando controler

--- routes/web.php
+++ routes/web.php
@@ -16,45 +16,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*Route::get('/teste', function(){
-    return "Olá! Seja bem vindo!";
-});
+Route::get('produtos', 'MeuControlador@produtos');
+Route::get('nome', 'MeuControlador@getNome');
+Route::get('idade', 'MeuControlador@getIdade');
+Route::get('multiplicar/{n1}/{n2}', 'MeuControlador@multiplicar');
 
-Route::get('/{nome}', function($nome){
-    return "Olá! Seja bem vindo, " . $nome . "!";
-});
-
-Route::get('/rotacomregras/{nome}/{n}', function($nome, $n){
-    for($i = 0; $i<$n; $i++)
-        echo "olá, seja bem vindo, $nome! <br>";
-})->where('nome','[A-Za-z]+')->where('n','[0-9]+');*/ //regras para rota
-
-Route::prefix('/app')->group(function(){
-    Route::get('/', function(){
-        return view ("app");
-    })->name('app');
-    
-    Route::get('/user', function(){
-        return view ("User");
-    })->name('app.user');
-
-    Route::get('/profile', function(){
-        return view ("Profile");
-    })->name('app.profile');
-});
-
-Route::get('/produtos', function() {
-    echo "<h1>Produtos</h1>";
-    echo "<ol>";
-    echo "<li>Notebook</li>";
-    echo "<li>Impressora</li>";
-    echo "<li>Mouse</li>";
-    echo "</ol>";
-})->name('meusprodutos');
-
-/////////////////////////////////////////
-
-Route::post('/requisicoes', function(Request $request) {
-    return "Hello POST";
-});
-///////
