5942d7218e792e5c5b2c8f6632d1085179fcb271
Sergio Santos <josesergio.s@hotmail.com>
2020-01-10 17:52:19 -0300

    aula 35

--- resources/views/layouts/principal.blade.php
+++ resources/views/layouts/principal.blade.php
@@ -2,9 +2,24 @@
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
-    <title>Título da página</title>
+    <title>Views</title>
+    <link rel="stylesheet" href="{{ asset('css/principal.css') }}">
   </head>
   <body>
-      @yield('conteudo')
+      <div class="row">
+          <div class="col1">
+            <div class="menu">
+                <ul>
+                  <li><a class="active" href="{{route('clientes.index')}}">Clientes</a></li>
+                      <li><a href="">Produtos</a></li>
+                      <li><a href="">Departamentos</a></li>
+                  </ul>  
+            </div>
+          </div>
+          <div class="col2">
+              @yield('conteudo')
+          </div>
+      </div>             
+      
   </body>
 </html>
